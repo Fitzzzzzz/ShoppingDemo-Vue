7676ca51771a46297f612fbe0bcc67f10b0dd13e
Fitzzzzzz <fitzz_wang@163.com>
2017-03-25 20:49:21 +0800

    totalPrice

--- ShoppingDemo/src/components/ShoppingCart.vue
+++ ShoppingDemo/src/components/ShoppingCart.vue
@@ -1,8 +1,9 @@
 <template>
   <div>
-    <mu-table :multiSelectable="multiSelectable" :enableSelectAll="enableSelectAll">
+    <mu-table :selectable="false" :showCheckbox="false">
         <mu-thead>
             <mu-tr>
+            <mu-th>选择</mu-th>
             <mu-th>ID</mu-th>
             <mu-th>商品名</mu-th>
             <mu-th>单价</mu-th>
@@ -11,7 +12,8 @@
             </mu-tr>
         </mu-thead>
         <mu-tbody>
-            <mu-tr v-for="(item, index) in goods" :key="item.id" :selectable="selectable" :selected="selected[index]">
+            <mu-tr v-for="(item, index) in goods" :key="item.id" :selected="selected[index]">
+            <mu-td><mu-checkbox :value="selected[index]" @input="selectCheckBox(index)"></mu-checkbox></mu-td>
             <mu-td>{{item.id}}</mu-td>
             <mu-td>{{item.title}}</mu-td>
             <mu-td>{{item.price}}</mu-td>
@@ -21,8 +23,8 @@
                 <mu-icon-button icon="add" @click="add(index)"></mu-icon-button>
             </mu-td>
             <mu-td>{{item.price * item.count}}</mu-td>
-            </mu-tr>            
-        </mu-tbody>        
+            </mu-tr>
+        </mu-tbody>
     </mu-table>
     <div class="pay">
         <mu-row>
@@ -40,10 +42,7 @@
             return {
                 goods : [],
                 disable : false,
-                enableSelectAll : true,
-                multiSelectable : true,
-                selectable : true,
-                selected : [true,true,true,false,true,false,true,false],
+                selected : [false,false,false,false,false,false,false,false],
                 totalPrice : 0
             }
         },
@@ -51,26 +50,30 @@
             this.$http.get('../../static/server/shoppingcart.json').then(response => {
                 this.goods = response.body;
                 console.log(response.body);
-                for(let i = 0;i < response.body.length;i++){
-                    this.totalPrice += response.body[i].price * response.body[i].count;
-                }
             });
         },
         methods: {
             add(index){
                 this.goods[index].count++;
-                this.totalPrice += Number(this.goods[index].price);
+                if(this.selected[index] === true){
+                  this.totalPrice += Number(this.goods[index].price);
+                }
                 if(this.goods[index].count > 1) this.disable = false;
-                this.selected[index] = false;
             },
             subtract(index){
                 this.goods[index].count--;
                 if(this.goods[index].count < 1) this.goods[index].count = 1;
-                else{
-                    this.totalPrice -= this.goods[index].price;
+                if(this.selected[index] === true){
+                  this.totalPrice -= Number(this.goods[index].price);
                 }
-                
-                this.selected[index] = false;
+            },
+            selectCheckBox(index){
+              this.selected[index] = !this.selected[index];
+              if(this.selected[index] === true){
+                this.totalPrice += Number(this.goods[index].price * this.goods[index].count);
+              }else{
+                this.totalPrice -= Number(this.goods[index].price * this.goods[index].count);
+              }
             }
         }
     }
@@ -79,6 +82,6 @@
 <style>
     .pay{
         text-align:center
-        
+
     }
 </style>
